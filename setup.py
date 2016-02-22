018cf76379a4971f17c6757bab03645b1947929e
Allen Guo <guoguo12@gmail.com>
2016-02-22 03:19:27 -0800

    Bump up to 1.1.0
    I think I'll start following semantic versioning (http://semver.org)
    more closely from now on. So, increment minor version number for
    new functionality, increment patch version number for bugfixes.
    
    Thanks @jameswenzel for anticipating a version bump though!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='billboard.py',
-      version='1.0.5',
+      version='1.1.0',
       description='Unofficial Python API for accessing Billboard.com charts',
       author='Allen Guo',
       author_email='guoguo12@gmail.com',
