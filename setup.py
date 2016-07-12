1f1e486d320be401b81e2ba9706bc46efd7d489d
Allen Guo <guoguo12@gmail.com>
2016-07-12 11:09:49 -0700

    Explicitly set required BS4 version (see #10)

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(name='billboard.py',
-      version='2.0.2',
+      version='2.0.3',
       description='Unofficial Python API for accessing Billboard.com charts',
       author='Allen Guo',
       author_email='guoguo12@gmail.com',
       url='https://github.com/guoguo12/billboard-charts',
       py_modules=['billboard'],
       license='MIT License',
-      install_requires=['beautifulsoup4', 'requests >= 2.2.1']
+      install_requires=['beautifulsoup4 >= 4.4.1', 'requests >= 2.2.1']
       )
