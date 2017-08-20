3972375c395b00ca306b7788809b7ec1c7cefd25
Allen Guo <guoguo12@gmail.com>
2017-08-20 11:44:51 -0700

    Add date rounding test

--- tests/test_billboard.py
+++ tests/test_billboard.py
@@ -58,10 +58,25 @@ class HistoricalHot100Test(CurrentHot100Test):
             assert str(self.chart) == reference.read()
 
 
+class DateRoundingTest(unittest.TestCase):
+    """Checks that the Billboard website is rounding dates correctly: it should
+    round up to the nearest date on which a chart was published.
+    """
+
+    def setUp(self):
+        self.chart = billboard.ChartData('hot-100', date='1000-10-10')
+
+    def test_correct_fields(self):
+        assert self.chart.date == '1958-08-04'  # The first Hot 100 chart
+
+
 class CurrentGreatestHot100SinglesTest(unittest.TestCase):
     """Checks that the ChartData object for the current
     greatest-hot-100-singles chart has all valid fields, and that its entries
     also have valid fields.
+
+    The greatest-hot-100-singles chart is special in that it does not provide
+    peak/last position or weeks-on-chart data.
     """
 
     def setUp(self):
