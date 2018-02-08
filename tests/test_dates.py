b7a6fb9646367eb929a4e6fc2f61d29108c81a8d
Allen Guo <guoguo12@gmail.com>
2018-02-08 14:10:11 -0800

    Improve date tests

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -5,25 +5,21 @@ import unittest
 import billboard
 
 
-class DateRoundingTest(unittest.TestCase):
+def test_date_rounding():
     """Checks that the Billboard website is rounding dates correctly: it should
     round up to the nearest date on which a chart was published.
     """
+    chart = billboard.ChartData('hot-100', date='1000-10-10')
+    assert chart.date == '1958-08-04'  # The first Hot 100 chart
 
-    def setUp(self):
-        self.chart = billboard.ChartData('hot-100', date='1000-10-10')
-
-    def test_correct_fields(self):
-        assert self.chart.date == '1958-08-04'  # The first Hot 100 chart
+    chart = billboard.ChartData('hot-100', date='1996-07-30')
+    assert chart.date == '1996-08-03'
 
 
-class DatetimeTest(unittest.TestCase):
+def test_datetime_date():
     """Checks that ChartData correctly handles datetime objects as the
     date parameter.
     """
-
-    def setUp(self):
-        self.chart = billboard.ChartData('hot-100', datetime.date(2016, 7, 8))
-
-    def test_successful_load(self):
-        self.assertTrue(len(self.chart) > 0)
+    chart = billboard.ChartData('hot-100', datetime.date(2016, 7, 9))
+    assert len(chart) == 100
+    assert chart.date == '2016-07-09'
