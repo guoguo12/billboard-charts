73d9c17ee4a98a3fb0c9944bdd46ead6292d6861
Allen Guo <guoguo12@gmail.com>
2018-04-27 01:36:55 -0700

    Bump to 4.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='billboard.py',
-      version='4.3.0',
+      version='4.4.0',
       description='Unofficial Python API for accessing Billboard.com charts',
       author='Allen Guo',
       author_email='guoguo12@gmail.com',
