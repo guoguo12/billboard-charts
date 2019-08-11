7c722ad788fb1d3097a0603d62ef882ca2d2cd3d
Allen Guo <guoguo12@gmail.com>
2019-08-11 17:35:27 -0400

    Fix indentation

--- tests/test_historical_charts.py
+++ tests/test_historical_charts.py
@@ -19,25 +19,23 @@ class Base:
         with open(referencePath) as reference:
             reference = json.loads(reference.read())
 
-            self.assertEqual(self.chart.name, reference["name"])
-            self.assertEqual(self.chart.title, reference["title"])
-            self.assertEqual(self.chart.date, reference["date"])
-            self.assertEqual(self.chart.previousDate, reference["previousDate"])
-            self.assertEqual(self.chart.nextDate, reference["nextDate"])
+        self.assertEqual(self.chart.name, reference["name"])
+        self.assertEqual(self.chart.title, reference["title"])
+        self.assertEqual(self.chart.date, reference["date"])
+        self.assertEqual(self.chart.previousDate, reference["previousDate"])
+        self.assertEqual(self.chart.nextDate, reference["nextDate"])
 
-            self.assertEqual(len(self.chart.entries), len(reference["entries"]))
-            for chartEntry, referenceEntry in zip(
-                self.chart.entries, reference["entries"]
-            ):
-                # We intentionally don't check image here, since image URLs
-                # might change a lot
-                self.assertEqual(chartEntry.title, referenceEntry["title"])
-                self.assertEqual(chartEntry.artist, referenceEntry["artist"])
-                self.assertEqual(chartEntry.peakPos, referenceEntry["peakPos"])
-                self.assertEqual(chartEntry.lastPos, referenceEntry["lastPos"])
-                self.assertEqual(chartEntry.weeks, referenceEntry["weeks"])
-                self.assertEqual(chartEntry.isNew, referenceEntry["isNew"])
-                self.assertEqual(chartEntry.rank, referenceEntry["rank"])
+        self.assertEqual(len(self.chart.entries), len(reference["entries"]))
+        for chartEntry, referenceEntry in zip(self.chart.entries, reference["entries"]):
+            # We intentionally don't check image here, since image URLs might
+            # change a lot
+            self.assertEqual(chartEntry.title, referenceEntry["title"])
+            self.assertEqual(chartEntry.artist, referenceEntry["artist"])
+            self.assertEqual(chartEntry.peakPos, referenceEntry["peakPos"])
+            self.assertEqual(chartEntry.lastPos, referenceEntry["lastPos"])
+            self.assertEqual(chartEntry.weeks, referenceEntry["weeks"])
+            self.assertEqual(chartEntry.isNew, referenceEntry["isNew"])
+            self.assertEqual(chartEntry.rank, referenceEntry["rank"])
 
 
 class TestHistoricalHot100(Base, unittest.TestCase):
