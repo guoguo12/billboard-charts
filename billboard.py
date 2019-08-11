01e480d2ebeb770f5db0a80051329a2d02a8bf5e
Allen Guo <guoguo12@gmail.com>
2019-08-11 10:44:45 -0400

    Fix lastPos
    We've been getting this wrong for some time. Billboard.com has two
    element matching the selector we use. We want the second match, not the
    first.

--- billboard.py
+++ billboard.py
@@ -268,12 +268,15 @@ class ChartData:
             def getPositionRowValue(rowName, ifNoValue=None):
                 try:
                     selector = _ROW_SELECTOR_FORMAT % rowName
-                    selected = entrySoup.select_one(selector)
-                    if selected is None or selected.string is None \
-                       or selected.string == '-':
+                    selected = entrySoup.select(selector)
+                    # We get the last element of selected because there are two
+                    # elements matching _LAST_POS_FORMAT and we want the second
+                    # one (the first is the position two weeks previous)
+                    if not selected or selected[-1].string is None \
+                       or selected[-1].string == '-':
                         return ifNoValue
                     else:
-                        return int(selected.string.strip())
+                        return int(selected[-1].string.strip())
                 except:
                     message = "Failed to parse row value: %s" % rowName
                     raise BillboardParseException(message)
