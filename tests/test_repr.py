2c261669bef6aa9b73305eb07c4c6d558b1c0284
Allen Guo <guoguo12@gmail.com>
2019-10-15 22:16:24 -0400

    Merge tests to reduce number of HTTP requests

--- tests/test_repr.py
+++ tests/test_repr.py
@@ -4,18 +4,21 @@ import six
 
 
 class ReprTest(unittest.TestCase):
+    """Checks that the string representations of charts and entries are correct.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.chart = billboard.ChartData("hot-100", date="2010-01-02")
+
     def testReprChart(self):
-        """Checks that the string representation of a chart is correct."""
-        chart = billboard.ChartData("hot-100", date="1996-08-03")
         self.assertEqual(
-            repr(chart), "billboard.ChartData('hot-100', date='1996-08-03')"
+            repr(self.chart), "billboard.ChartData('hot-100', date='2010-01-02')"
         )
 
     def testReprEntry(self):
-        """Checks that the string representation of an entry is correct."""
-        chart = billboard.ChartData("hot-100", date="2010-01-02")
         self.assertEqual(
-            repr(chart[0]),
+            repr(self.chart[0]),
             "billboard.ChartEntry(title={!r}, artist={!r})".format(
                 six.text_type("TiK ToK"), six.text_type("Ke$ha")
             ),
