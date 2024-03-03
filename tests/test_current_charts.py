dd90166ce2aff1c4ab1f86dfbf3719a69e45f478
Allen Guo <guoguo12@gmail.com>
2024-03-03 12:06:04 -0500

    Uncomment regression test for image parsing

--- tests/test_current_charts.py
+++ tests/test_current_charts.py
@@ -28,8 +28,7 @@ class Base:
             if not skipTitleCheck:
                 self.assertGreater(len(entry.title), 0)
             self.assertGreater(len(entry.artist), 0)
-            # TODO: Add this check back after we can parse images
-            # self.assertGreater(len(entry.image), 0)
+            self.assertGreater(len(entry.image), 0)
             self.assertTrue(1 <= entry.peakPos <= self.expectedNumEntries)
             self.assertTrue(0 <= entry.lastPos <= self.expectedNumEntries)
             self.assertGreaterEqual(entry.weeks, 1)
