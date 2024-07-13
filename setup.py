7145262220a7ecb82ae5cec45414c51f98f83c59
Allen Guo <guoguo12@gmail.com>
2024-07-13 15:45:18 -0400

    Try to set description on pypi.org

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@
 
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="billboard.py",
     version="7.0.1",  # Don't forget to update CHANGELOG.md!
     description="Python API for downloading Billboard charts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Allen Guo",
     author_email="guoguo12@gmail.com",
     url="https://github.com/guoguo12/billboard-charts",
